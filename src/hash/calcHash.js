4a433aa2a790408b918e29d27a1ba72ede5e95a5
eugeniakhaleeva <khaleeva20990@gmail.com>
2024-10-03 23:46:06 +0200

    feat: implement fn calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import {createHash} from "node:crypto";
+import {createReadStream} from "node:fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt");
+    const hash = createHash('sha256');
+
+    const input = createReadStream(filePath);
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+            hash.update(data);
+        else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
+
