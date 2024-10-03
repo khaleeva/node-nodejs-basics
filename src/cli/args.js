c0f483fa59af24d9f7dc3ae801add901eaa54040
eugeniakhaleeva <khaleeva20990@gmail.com>
2024-10-04 01:42:51 +0200

    refactor: change check condition in fn args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,8 +2,8 @@ const parseArgs = () => {
     const args = process.argv.slice(2);
     const data = args.reduce((arr, value, index) => {
         let key;
-        if (index % 2 === 0) {
-            key = index
+        if (value.startsWith('--')) {
+            key = args.indexOf(value)
             arr.push({[index]: `${args[key].slice(2)} is ${args[key + 1]}`});
             return arr;
         }
