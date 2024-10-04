66b0489194d59970061b0d5287e9448986453f04
eugeniakhaleeva <khaleeva20990@gmail.com>
2024-10-04 14:54:40 +0200

    feat: implement fn write.js in streams module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import path from "path";
+import {createWriteStream} from "fs";
+const { stdin } = process;
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(import.meta.dirname, "files", 'fileToWrite.txt')
+
+    const output = createWriteStream(filePath);
+
+    stdin.on("data", (data) => {
+        output.write(data);
+    });
+
+    stdin.on("end", () => {
+        output.end();
+    });
 };
 
 await write();
