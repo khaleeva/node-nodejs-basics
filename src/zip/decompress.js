1841ccfa553be845fb5fe2dff116d39862f69f43
eugeniakhaleeva <khaleeva20990@gmail.com>
2024-10-05 00:54:44 +0200

    feat: implement fn decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import {join} from "path";
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToCompress.txt')
+    const compressFilePath = join(import.meta.dirname, 'files', 'archive.gz')
+
+    if (!fs.existsSync(compressFilePath)) {
+        console.error('FS operation failed');
+    } else{
+        const input = fs.createReadStream(compressFilePath);
+        const output = fs.createWriteStream(filePath);
+        const unzip = zlib.createUnzip();
+
+        input.pipe(unzip).pipe(output);
+    }
+
+
+
 };
 
 await decompress();
